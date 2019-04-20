f932af973bd7dcd126dd62fe88a75529c51c844b
Sedat GÜZELŞEMME <sedat.guzelsemme@antasya.com>
2019-04-20 14:25:36 +0300

    initial commit

---
+++ CPP/HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+	Ctrl+F7 build the file individually
+	Ctrl+F5 Run Run builded executable file
+	F5 start Debugging session
+
+*/
+
+#include <iostream>
+
+int main() {
+	using namespace std;
+
+	cout << "Hello World" << endl;
+
+	cin.get();
+
+	return 0;
+}
